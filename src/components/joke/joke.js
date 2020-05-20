bb6a2f176c8ce4a48bcbd6008c5898edf677bfc9
kosssm155 <kosssm155@gmail.com>
2020-05-20 03:34:13 +0300

    add remove likes

---
+++ src/components/joke/joke.js
@@ -0,0 +1,53 @@
+export default class Joke {
+  constructor({
+    categories = 'undefined',
+    created_at = 'undefined',
+    icon_url = 'undefined',
+    id = 'undefined',
+    updated_at = 'undefined',
+    url = 'undefined',
+    value = 'undefined',
+  }) {
+    this.categories = categories.length === 0 ? 'random' : categories[0];
+    this.created_at = created_at;
+    this.icon_url = icon_url;
+    this.id = id;
+    this.updated_at = updated_at;
+    this.url = url;
+    this.value = value;
+  }
+
+  renderJoke() {
+    let jokeContainer = document.createElement('div');
+    jokeContainer.className = `joke ${this.id}`;
+    jokeContainer.innerHTML = `
+    <div class="joke__inner">
+      <p class="joke__id"><span>ID:</span><a href="${this.url}">${this.id}</a></p>
+      <p class="joke__text">
+        ${this.value}
+      </p>
+      <div class="joke__footer">
+        <span class="joke__last-update">Last update: ${this.updated_at}</span>
+        <span class="joke__category">${this.categories}</span>
+      </div>
+    </div>
+    <div class="joke__outer">
+      <div class="joke__like ${this.id}"></div>
+      <div class="joke__message"></div>
+    </div>
+    `;
+
+    document.getElementById('layout__jokes').appendChild(jokeContainer);
+  }
+
+  // listenLikes() {
+  //   let generatedJokesLikeList = document.querySelectorAll('.joke__like');
+  //   generatedJokesLikeList.forEach((like) => {
+  //     console.log('like', like);
+  //     // like.addEventListener('click', function () {
+  //     //   this.style.backgroundImage = "url('./heart.png')";
+  //     //   console.log(document.querySelector(`.joke ${this.id}`));
+  //     // });
+  //   });
+  // }
+}
--- src/components/submit/submit.js
+++ src/components/submit/submit.js
@@ -1,6 +1,14 @@
+import Joke from '../joke/joke.js';
+
+// Button class functions:
+// 1. Get JSON data as a string
+// 2. Fetch url
+// 3. Listen when user like joke
+
 export default class Button {
   constructor() {
     this.jsonVal = {};
+    this.jsonValArrOfObjects = [];
   }
 
   // add event listener to buttor and return fetch object
@@ -10,7 +18,34 @@ export default class Button {
       e.preventDefault();
       this.getJSONData(filter.finalFilterValue).then((data) => {
         this.jsonVal = data;
-        console.log(this);
+
+        if (this.jsonVal.total) {
+          this.jsonValArrOfObjects = [];
+          this.jsonValArrOfObjects = this.jsonVal.result;
+        } else {
+          this.jsonValArrOfObjects = [];
+          this.jsonValArrOfObjects.push(this.jsonVal);
+        }
+
+        document.getElementById('layout__jokes').innerHTML = '';
+
+        this.jsonValArrOfObjects.forEach((element) => {
+          let joke = new Joke(element);
+          joke.renderJoke();
+        });
+
+        let layoutJokes = document.getElementById('layout__jokes');
+        let jokesNodeList = layoutJokes.querySelectorAll('.joke');
+
+        jokesNodeList.forEach((element) => {
+          element.addEventListener('click', (e) => {
+            if (e.target.classList[2] === 'heart-active') {
+              e.target.classList.remove('heart-active');
+            } else if (e.target.className.split(' ')[0] === 'joke__like') {
+              e.target.classList.add('heart-active');
+            }
+          });
+        });
       });
     });
   }
@@ -25,4 +60,10 @@ export default class Button {
       }
     });
   }
+
+  // handleJoke(e) {
+  //   console.log(this);
+  //   console.log(e);
+  //   let like = this.querySelectorAll('.joke__like');
+  // }
 }
